c0245335545602fc10a5ff08a233637c7db3dd81
suriya_muthukumar <86966632+suriya1776@users.noreply.github.com>
2025-03-26 15:26:59 +0530

    Update Consumer.cs

--- src/accounting/Consumer.cs
+++ src/accounting/Consumer.cs
@@ -25,7 +25,7 @@ internal class Consumer : IDisposable
         _consumer = BuildConsumer(servers);
         _consumer.Subscribe(TopicName);
 
-        _logger.LogInformation($"Connecting to Kafka2: {servers}");
+        _logger.LogInformation($"Connecting to Kafka3: {servers}");
     }
 
     public void StartListening()
